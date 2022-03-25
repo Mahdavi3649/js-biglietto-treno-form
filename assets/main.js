4e820837f6bc609b0cbd3db5de1f2ef48ba57cc4
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-25 15:40:40 +0100

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+
+
+ // get values from inputs
+ const name = document.getElementById("name").value;
+ const km = document.getElementById("km").value;
+ const age = document.getElementById("age").value;
