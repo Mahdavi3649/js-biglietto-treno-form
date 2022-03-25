995356c960691507ce385b398bc14477f7ca48f7
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-25 17:17:04 +0100

    aggiustanto alcune parte

--- assets/main.js
+++ assets/main.js
@@ -7,28 +7,42 @@ enter.addEventListener("click", function(){
     const km = document.getElementById("km").value;
     const age = document.getElementById("age").value;
 
+    console.log(name,km,age);
+
 
    // prezzo totale del viaggio per persone tra i 19 e 64 anni
    // 0.21€ al km
-    const costoBiglietto = km * 0.21;
-    console.log(costoBiglietto);
 
-    
+   
+   let prezzo_finale;
+   const prezzo_al_km = 0.21;
+   const prezzo_standard = km * prezzo_al_km;
+   //console.log(prezzo_standard);
+   
+   
+   const prezzo_minorenni = prezzo_standard - (prezzo_standard * 0.2)
+   const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
+
+
+
+
     const offerta = 'Tariffa Standard'
    // sconto
    // 20% under 18
    // 40% over 65
 
     if (age == 'minorenne'){
-        const calcoloScontoUnder = ((costoBiglietto * 20) / 100 );
-        const costoBiglietto = costoBiglietto - calcoloScontoUnder;
-   
+       /*  const calcoloScontoUnder = ((costoBiglietto * 20) / 100 );
+        const costoBiglietto = costoBiglietto - calcoloScontoUnder; */
+
+        prezzo_finale = prezzo_minorenni;
         offerta = 'Sconto Minorenne'
 
       }else if (age == 'over 65'){
-        const calcoloScontoOver = ((costoBiglietto * 40) / 100 );
-        const costoBiglietto = costoBiglietto - calcoloScontoOver;
-   
+       /*  const calcoloScontoOver = ((costoBiglietto * 40) / 100 );
+        const costoBiglietto = costoBiglietto - calcoloScontoOver; */
+
+        prezzo_finale = prezzo_over_65;
         offerta = 'Sconto Over 65'
       }
       const carrozza = Math.floor(Math.random()*9) + 1;
@@ -37,7 +51,7 @@ enter.addEventListener("click", function(){
     
        document.getElementById('nome_passeggero').innerHTML = name;
        document.getElementById('offerta').innerHTML = offerta;
-       document.getElementById('costo').innerHTML = costoBiglietto.toFixed(2);
+       document.getElementById('costo').innerHTML = prezzo_finale.toFixed(2);
        document.getElementById('codice-cp').innerHTML = cp;
        document.getElementById('carrozza').innerHTML = carrozza;
     
