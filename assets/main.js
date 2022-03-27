f70cb6c577903f7a6312d49bfdffc2213f6b8a98
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-03-27 18:32:32 +0200

    Ottimizzato il Codice

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,65 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65. */
 
 const enter = document.getElementById("enter")
 enter.addEventListener("click", function(){
 
-    // get values from inputs
-    const name = document.getElementById("name").value;
-    const km = document.getElementById("km").value;
-    const age = document.getElementById("age").value;
+// get values from inputs
+const name = document.getElementById("name").value;
+const km = document.getElementById("km").value;
+const age = document.getElementById("age").value;
+//console.log(name,km,age);
 
-    //console.log(name,km,age);
+// prezzo totale del viaggio per persone tra i 19 e 64 anni
+const prezzo_al_km = 0.21;
+let prezzo_finale;
+const prezzo_standard = km * prezzo_al_km;
+//console.log(prezzo_standard);
+
+// prezzo per i minnorenni di uno sconto 20%
+const prezzo_minnoreni = prezzo_standard - (prezzo_standard * 0.2)
+
+//uno sconto del 40% per gli over 65
+const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
+//console.log(prezzo_minnoreni, prezzo_over_65);
+
+if (isNaN(km)) {
+  alert("devi inserire un numero per i km da percorrere")
+}
+
+if(age <= 18){
+   messaggio = "hai diritto al prezzo di scontato 20%";
+   prezzo_finale = prezzo_minnoreni
+
+}else if(age >= 65){
+   messaggio = "hai diritto al prezzo di scontato 40%";
+   prezzo_finale = prezzo_over_65
+} else{
+   messaggio = "mi dispace, nessun sconto applicabile";
+   prezzo_finale = prezzo_standard
+}
 
 
-   // prezzo totale del viaggio per persone tra i 19 e 64 anni
-   // 0.21€ al km
-   const costoBiglietto = km * 0.21;
-   console.log(costoBiglietto);
-
-   const offerta = 'Tariffa Standard'
-   // sconto
-   // 20% under 18
-   // 40% over 65
-   if (age == 'minorenne'){
-     const calcoloScontoUnder = ((costoBiglietto * 20) / 100 );
-     const costoBiglietto = costoBiglietto - calcoloScontoUnder;
-
-     offerta = 'Sconto Minorenne'
-   }else if (age == 'over 65'){
-     const calcoloScontoOver = ((costoBiglietto * 40) / 100 );
-     const costoBiglietto = costoBiglietto - calcoloScontoOver;
-
-     offerta = 'Sconto Over 65'
-   }
-      const carrozza = Math.floor(Math.random()*9) + 1;
-      const cp = Math.floor(Math.random() * (100000 - 90000 + 1)) +90000;
-    
-    
-       document.getElementById('nome_passeggero').innerHTML = name;
-       document.getElementById('offerta').innerHTML = offerta;
-       document.getElementById('costo').innerHTML = costoBiglietto.toFixed(2);
-       document.getElementById('codice-cp').innerHTML = cp;
-       document.getElementById('carrozza').innerHTML = carrozza;
-    
-})
+     const carrozza = Math.floor(Math.random()*9) + 1;
+     const cp = Math.floor(Math.random() * 100000) + 1;
+   
+      document.getElementById('nome_passeggero').innerHTML = name;
+      document.getElementById('offerta').innerHTML = `SCONTO ${age.toUpperCase()}`;
+      document.getElementById('costo').innerHTML = prezzo_finale.toFixed(2);
+      if (age ==='maggiorenne') {
+        document.getElementById('offerta').innerHTML = `SCONTO NON DISPONIBILE`
+    }
+    if(isNaN(prezzo_finale)){
+        document.getElementById('prezzo_finale').innerHTML = `RICONTROLLA I DATI INSERITI`
+    }
+      document.getElementById('codice-cp').innerHTML = cp;
+      document.getElementById('carrozza').innerHTML = carrozza;
+});
 
 const reset = document.getElementById('reset');
-reset.addEventListener('click',
- function(){
+reset.addEventListener('click',function(){
    document.getElementById('nome_passeggero').innerHTML = "";
    document.getElementById('offerta').innerHTML = "";
    document.getElementById('costo').innerHTML = "";
@@ -53,7 +68,4 @@ reset.addEventListener('click',
    document.getElementById('name').value = "";
    document.getElementById('age').value = "";
    document.getElementById('km').value = "";
- }
-
-);
- 
+ });
